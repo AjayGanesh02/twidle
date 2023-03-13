d8626af3d97d8bb5a2fcd3a1e3085f257a9612bb
Ajay Ganesh <ajganesh2002@gmail.com>
2023-03-12 22:18:04 -0400

    updated schema to support task model

--- src/env.mjs
+++ src/env.mjs
@@ -21,6 +21,7 @@ const server = z.object({
   // Add `.min(1) on ID and SECRET if you want to make sure they're not empty
   DISCORD_CLIENT_ID: z.string(),
   DISCORD_CLIENT_SECRET: z.string(),
+  OPENAI_API_KEY: z.string()
 });
 
 /**
@@ -44,6 +45,7 @@ const processEnv = {
   NEXTAUTH_URL: process.env.NEXTAUTH_URL,
   DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
   DISCORD_CLIENT_SECRET: process.env.DISCORD_CLIENT_SECRET,
+  OPENAI_API_KEY: process.env.OPENAI_API_KEY
   // NEXT_PUBLIC_CLIENTVAR: process.env.NEXT_PUBLIC_CLIENTVAR,
 };
 
