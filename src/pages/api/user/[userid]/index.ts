e795cdaee4b0e6016587ff9b68a22916fe36b725
Ajay Ganesh <ajganesh2002@gmail.com>
2023-03-16 00:56:27 -0400

    worked on routes and changed schema (screw reordering)

--- src/pages/api/user/[userid]/index.ts
+++ src/pages/api/user/[userid]/index.ts
@@ -1,15 +1,22 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "~/server/db";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    if (req.method == "GET") {
-        if (!req.query.userid) { 
-            return res.json({"error": "Specify a user id"})
-        }
-      const resp = await prisma.user.findUnique({
-          where: {
-            id: typeof(req.query.userid) == "string" ? req.query.userid:req.query.userid[0]
-          }
-      })
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method == "GET") {
+    if (!req.query.userid) {
+      return res.json({ error: "Specify a user id" });
+    }
+    const users = await prisma.user.findUnique({
+      where: {
+        id:
+          typeof req.query.userid == "string"
+            ? req.query.userid
+            : req.query.userid[0],
+      },
+    });
+    res.status(200).json(users);
   }
 }
--- src/pages/api/user/[userid]/tasks/index.ts
+++ src/pages/api/user/[userid]/tasks/index.ts
@@ -1,8 +1,26 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "~/server/db";
+import { z } from "zod";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+const taskSchema = z.object({
+  userid: z.string(),
+  title: z.string(),
+  description: z.string().optional(),
+})
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method == "GET") {
-    console.log(prisma.task.count());
+    if (typeof req.query.userId != "string") {
+      return res.status(400).json({ error: "Specify a userid" });
+    }
+    const tasks = await prisma.task.findMany({
+      where: {
+        userId: typeof req.query.userid === "string" ? req.query.userid : "",
+      },
+    });
+
+    return res.status(200).json(tasks);
+  } else if (req.method == "POST") { 
+
   }
 }
