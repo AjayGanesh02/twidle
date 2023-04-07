4a63b313a0317031e5279795155189b89efa528f
Ajay Ganesh <ajganesh2002@gmail.com>
2023-04-07 18:00:05 -0400

    schema edit and finished basic route for creating new task

--- src/pages/api/user/[userid]/tasks/index.ts
+++ src/pages/api/user/[userid]/tasks/index.ts
@@ -3,16 +3,18 @@ import { prisma } from "~/server/db";
 import { z } from "zod";
 
 const taskSchema = z.object({
-  userid: z.string(),
   title: z.string(),
   description: z.string().optional(),
-})
+});
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (typeof req.query.userid != "string") {
+    return res.status(400).json({ Error: "Specify a userid" });
+  }
   if (req.method == "GET") {
-    if (typeof req.query.userId != "string") {
-      return res.status(400).json({ error: "Specify a userid" });
-    }
     const tasks = await prisma.task.findMany({
       where: {
         userId: typeof req.query.userid === "string" ? req.query.userid : "",
@@ -20,7 +22,20 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     });
 
     return res.status(200).json(tasks);
-  } else if (req.method == "POST") { 
+  } else if (req.method == "POST") {
+    const resp = taskSchema.safeParse(req.body);
+    if (!resp.success) {
+      return res.status(400).json({ Error: "Malformed Request" });
+    } else {
+      const task = await prisma.task.create({
+        data: {
+          user: { connect: { id: req.query.userid } },
+          title: resp.data.title,
+          description: resp.data.description || "",
+        },
+      });
 
+      return res.status(200).json(task);
+    }
   }
 }
